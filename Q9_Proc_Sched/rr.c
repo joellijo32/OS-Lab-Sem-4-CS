72b856f951efcf0ae62a4393469ba83e59de8851
Joel Lijo Mathew <joellijo4@gmail.com>
2026-01-06 16:40:42 +0000

    Round robin almost completed - q9

--- Q9_Proc_Sched/rr.c
+++ Q9_Proc_Sched/rr.c
@@ -6,8 +6,8 @@ typedef struct Proc{
 
 int main(){
 	printf("\nNumber of Processes: ");
-	int n;
-	scanf("%d", &n); Proc p[n]; int tq = 3;int completed = 0;, current_time = 0;
+	int n, tat_sum =0, wait_sum = 0;
+	scanf("%d", &n); Proc p[n]; int tq = 3;int completed = 0,current_time = 0;
 	printf("\nEnter Details of %d proccess:\n", n);
 	for(int i = 0; i < n;i++){
 		printf("\nProccess %d:\n", i+1);
@@ -15,10 +15,9 @@ int main(){
 		printf("\tArrival: "); scanf("%d", &(p[i].arrival) );
 		printf("\tBurst: "); scanf("%d", &(p[i].burst) );
 		p[i].remaining = p[i].burst;
-		printf("\tPriority: "); scanf("%d", &(p[i].priority) );
 		printf("Proccess %d Registered.\n", i+1);
 	}
-	Proc small; int pos;
+	Proc small; int pos, gantt[1000];
 	for(int i = 0; i< n-1;i++){
 		small = p[i];pos = i;
 		for(int j = i+1; j < n;j++){
@@ -29,10 +28,12 @@ int main(){
 		p[pos] = p[i];
 		p[i] = small;
 	}
-
+	pos = 0;int flag = 0;
 	while(completed < n){
 		for(int i = 0;i < n;i++){
 			if(p[i].arrival <= current_time && p[i].remaining > 0){
+				gantt[pos++] = p[i].id;
+				flag = 1;
 				if(p[i].remaining > 3){
 					p[i].remaining = p[i].remaining - 3;
 					current_time += 3;
@@ -40,10 +41,26 @@ int main(){
 					current_time += p[i].remaining;
 					p[i].remaining = 0;
 					completed++;
+                    			current_time++;
+              				p[i].completion = current_time;
+					p[i].turnaround = p[i].completion - p[i].arrival;
+					p[i].waiting = p[i].turnaround  - p[i].burst;
+
+					wait_sum += p[i].waiting;
+					tat_sum += p[i].turnaround;
 				}
 			}
 		}
+		if(flag == 0) current_time++;
 	}
+
+	printf("\nRound Robin Scheduling Complete.\n");
+	printf("\nAverage Waiting Time: %lf", (double)wait_sum/n);
+	printf("\nAverage Turn Around Time: %lf", (double)tat_sum/n);
+	printf("\n\nGantt Chart: \n");
+	for(int i = 0;i < pos;i++){
+		printf("| %d ", gantt[i]);
+	}printf("|\n");
 
 
         return 0;
