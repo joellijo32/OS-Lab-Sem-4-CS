50ddb87ca5be7e47334b6867b9b730b6d2ddebc9
Joel Lijo Mathew <joellijo4@gmail.com>
2026-01-06 15:55:46 +0000

    Started round robin , input & sorting complete- q9

---
+++ Q9_Proc_Sched/rr.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+typedef struct Proc{
+	int id, arrival, burst, completion, waiting, turnaround, remaining;
+}Proc ;
+
+int main(){
+	printf("\nNumber of Processes: ");
+	int n;
+	scanf("%d", &n); Proc p[n]; int tq = 3;int completed = 0;, current_time = 0;
+	printf("\nEnter Details of %d proccess:\n", n);
+	for(int i = 0; i < n;i++){
+		printf("\nProccess %d:\n", i+1);
+		printf("\tID: "); scanf("%d", &(p[i].id));
+		printf("\tArrival: "); scanf("%d", &(p[i].arrival) );
+		printf("\tBurst: "); scanf("%d", &(p[i].burst) );
+		p[i].remaining = p[i].burst;
+		printf("\tPriority: "); scanf("%d", &(p[i].priority) );
+		printf("Proccess %d Registered.\n", i+1);
+	}
+	Proc small; int pos;
+	for(int i = 0; i< n-1;i++){
+		small = p[i];pos = i;
+		for(int j = i+1; j < n;j++){
+			if(small.arrival > p[j].arrival){
+				small = p[j];pos = j;
+			}
+		}
+		p[pos] = p[i];
+		p[i] = small;
+	}
+
+
+        return 0;
+}
