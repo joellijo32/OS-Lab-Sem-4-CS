65e2f9450bf4132bc2ed8eca32e1e5d5bd77a132
Joel Lijo Mathew <joellijo4@gmail.com>
2026-01-13 18:20:40 +0000

    Added skeleton for q7 b

--- Q7_IPC/b_message_que.c
+++ Q7_IPC/b_message_que.c
@@ -7,5 +7,18 @@
 #include <sys/msg.h>
 #include <sys/wait.h>
 
+struct msg_buffer {
+	long msg_type;
+	char msg_text[100];
+};
 
 
+
+
+int main() {
+	key_t key;
+	int msgid;
+	struct msg_buffer message ;
+	pid_t pid;
+	return 0;
+}
