607eccd7e13d5c71fff574f9d638900d2fb009e0
Joel Lijo Mathew <joellijo4@gmail.com>
2026-02-11 00:52:27 +0530

    NOT CORRECT - Writers part partially done

--- Lab Cycle Solutions/Q10_Read_Write/Q10_Read_Write.c
+++ Lab Cycle Solutions/Q10_Read_Write/Q10_Read_Write.c
@@ -8,5 +8,15 @@ typedef struct Details{
 	sem_t mutex, w_mutex, rw_mutex, r_mutex;
 } details;
 
-
+void *write(void *args){
+	details *d = (details *)args;
+	sem_wait(&(d->w_mutex));
+	d->wc++;
+	if(d->wc == 1){
+		sem_wait(&(d->r_mutex));
+	}
+	//provided writer finished
+	sem_post(&(d->rw_mutex));
+	return d;
+}
 
