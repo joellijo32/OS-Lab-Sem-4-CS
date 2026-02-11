62f98c6b9b4f4df7a9db73d100da3e686c2672e0
Joel Lijo Mathew <joellijo4@gmail.com>
2026-02-11 12:43:01 +0530

    Q7 b created progfile and added main function going on

--- Lab Cycle Solutions/Q7_IPC/b_message_que.c
+++ Lab Cycle Solutions/Q7_IPC/b_message_que.c
@@ -7,18 +7,34 @@
 #include <sys/msg.h>
 #include <sys/wait.h>
 
-struct msg_buffer {
+typedef struct msg_buffer {
 	long msg_type;
 	char msg_text[100];
-};
+}msg_buffer ;
 
 
 
 
 int main() {
-	key_t key;
+	key_t msg_key;
 	int msgid;
-	struct msg_buffer message ;
+	msg_buffer message ;
 	pid_t pid;
+
+	msg_key = ftok("progfile", 65);
+	msgid = msgget(msg_key, 0666 | IPC_CREAT);
+
+	if(msgid == -1){
+		printf("\nMessage Que Failed\n");
+		return 1;
+	}
+
+	pid_t pid = fork();
+	if(pid == 0){
+		msgrcv(msgid, &message, sizeof(message), 1, 0);
+	}else {
+	}
+
+
 	return 0;
 }
