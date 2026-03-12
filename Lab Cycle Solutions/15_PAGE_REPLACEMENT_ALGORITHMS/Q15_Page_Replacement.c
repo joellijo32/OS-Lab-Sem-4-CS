fa58bb825cca500dafab5a5c75f4ca33e480ab38
Sreehari Vinayan <sreeharivinayan06@gmail.com>
2026-03-13 01:15:43 +0530

    Refactor code with matrix table print

--- Lab Cycle Solutions/15_PAGE_REPLACEMENT_ALGORITHMS/Q15_Page_Replacement.c
+++ Lab Cycle Solutions/15_PAGE_REPLACEMENT_ALGORITHMS/Q15_Page_Replacement.c
@@ -4,8 +4,8 @@
 
 #define MAX_PAGES 1000
 #define PAGE_RANGE 10
+#define MAX_FRAMES 7
 
-// Checks if page is in frames
 int search(int frames[], int frameCount, int page)
 {
     for (int i = 0; i < frameCount; i++)
@@ -16,15 +16,49 @@ int search(int frames[], int frameCount, int page)
     return -1;
 }
 
-// FIFO Page Replacement
-int fifo(int pages[], int n, int frameCount)
+void printMatrix(int matrix[][MAX_PAGES], int pages[], int frameCount, int n)
 {
-    int frames[frameCount];
+    for (int i = 0; i < n; i++)
+        printf("%d ", pages[i]);
+
+    printf("\n");
+
+    for (int i = 0; i < n * 2; i++)
+        printf("_");
+
+    printf("\n");
+
+    for (int i = 0; i < frameCount; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (matrix[i][j] == -1)
+                printf("- ");
+            else
+                printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void populateMatrixColumn(int matrix[][MAX_PAGES], int frames[], int frameCount, int columnNumber)
+{
+    for (int i = 0; i < frameCount; i++)
+        matrix[i][columnNumber] = frames[i];
+}
+
+int fifo(int pages[], int n, int frameCount, int matrix[][MAX_PAGES])
+{
+    int frames[MAX_FRAMES];
     int faults = 0;
     int index = 0;
 
     for (int i = 0; i < frameCount; i++)
         frames[i] = -1;
+
+    for (int i = 0; i < frameCount; i++)
+        for (int j = 0; j < n; j++)
+            matrix[i][j] = -1;
 
     for (int i = 0; i < n; i++)
     {
@@ -34,14 +68,16 @@ int fifo(int pages[], int n, int frameCount)
             index = (index + 1) % frameCount;
             faults++;
         }
+
+        populateMatrixColumn(matrix, frames, frameCount, i);
     }
+
     return faults;
 }
 
-// LRU Page Replacement
-int lru(int pages[], int n, int frameCount)
+int lru(int pages[], int n, int frameCount, int matrix[][MAX_PAGES])
 {
-    int frames[frameCount], time[frameCount];
+    int frames[MAX_FRAMES], time[MAX_FRAMES];
     int faults = 0, counter = 0;
 
     for (int i = 0; i < frameCount; i++)
@@ -49,6 +85,10 @@ int lru(int pages[], int n, int frameCount)
         frames[i] = -1;
         time[i] = 0;
     }
+
+    for (int i = 0; i < frameCount; i++)
+        for (int j = 0; j < n; j++)
+            matrix[i][j] = -1;
 
     for (int i = 0; i < n; i++)
     {
@@ -69,6 +109,7 @@ int lru(int pages[], int n, int frameCount)
                     replaceIndex = j;
                     break;
                 }
+
                 if (time[j] < minTime)
                 {
                     minTime = time[j];
@@ -80,30 +121,36 @@ int lru(int pages[], int n, int frameCount)
             time[replaceIndex] = ++counter;
             faults++;
         }
+
+        populateMatrixColumn(matrix, frames, frameCount, i);
     }
+
     return faults;
 }
 
-// Optimal Page Replacement
-int optimal(int pages[], int n, int frameCount)
+int optimal(int pages[], int n, int frameCount, int matrix[][MAX_PAGES])
 {
-    int frames[frameCount];
+    int frames[MAX_FRAMES];
     int faults = 0;
 
     for (int i = 0; i < frameCount; i++)
         frames[i] = -1;
 
+    for (int i = 0; i < frameCount; i++)
+        for (int j = 0; j < n; j++)
+            matrix[i][j] = -1;
+
     for (int i = 0; i < n; i++)
     {
         if (search(frames, frameCount, pages[i]) == -1)
         {
-
             int replaceIndex = -1;
             int farthest = i;
 
             for (int j = 0; j < frameCount; j++)
             {
                 int k;
+
                 for (k = i + 1; k < n; k++)
                 {
                     if (frames[j] == pages[k])
@@ -129,7 +176,10 @@ int optimal(int pages[], int n, int frameCount)
             frames[replaceIndex] = pages[i];
             faults++;
         }
+
+        populateMatrixColumn(matrix, frames, frameCount, i);
     }
+
     return faults;
 }
 
@@ -150,7 +200,9 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    int pages[n];
+    printf("Number of Frames: %d\n\n", frameCount);
+
+    int pages[MAX_PAGES];
 
     // For removing the problem of pseudo random repeated values
     srand(time(NULL));
@@ -162,16 +214,26 @@ int main(int argc, char *argv[])
         pages[i] = rand() % PAGE_RANGE;
         printf("%d ", pages[i]);
     }
-    printf("\n\n");
+    printf("\n");
 
-    int fifoFaults = fifo(pages, n, frameCount);
-    int lruFaults = lru(pages, n, frameCount);
-    int optimalFaults = optimal(pages, n, frameCount);
+    int matrix[MAX_FRAMES][MAX_PAGES];
 
-    printf("Number of Frames: %d\n\n", frameCount);
+    printf("\nFIFO:\n");
+    int fifoFaults = fifo(pages, n, frameCount, matrix);
+    printMatrix(matrix, pages, frameCount, n);
+
+    printf("\nLRU:\n");
+    int lruFaults = lru(pages, n, frameCount, matrix);
+    printMatrix(matrix, pages, frameCount, n);
+
+    printf("\nOptimal:\n");
+    int optimalFaults = optimal(pages, n, frameCount, matrix);
+    printMatrix(matrix, pages, frameCount, n);
+
+    printf("\nSummary:\n\n");
     printf("FIFO Page Faults: %d\n", fifoFaults);
     printf("LRU Page Faults: %d\n", lruFaults);
-    printf("Optimal Page Faults: %d\n", optimalFaults);
+    printf("Optimal Page Faults: %d\n\n", optimalFaults);
 
     return 0;
 }
