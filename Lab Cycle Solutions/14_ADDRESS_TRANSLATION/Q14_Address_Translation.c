664c732e5cf39d6530b379dd61f5ecd31df995fb
Joel Lijo Mathew <joellijo4@gmail.com>
2026-03-18 17:40:25 +0530

    Q14 spacing reduced

--- Lab Cycle Solutions/14_ADDRESS_TRANSLATION/Q14_Address_Translation.c
+++ Lab Cycle Solutions/14_ADDRESS_TRANSLATION/Q14_Address_Translation.c
@@ -1,53 +1,39 @@
 #include <stdio.h>
 
-int main(void)
-{
+int main(void) {
     long virt_addr_size;
     long page_size;
     long virt_addr;
-
     printf("Enter the virtual address space size (in MB): ");
     scanf("%ld", &virt_addr_size);
-
     printf("Enter the page size (in KB): ");
     scanf("%ld", &page_size);
-
     long page_size_bytes  = page_size * 1024;
     long total_pages      = (virt_addr_size * 1024 * 1024) / page_size_bytes;
 
     /* Page table: 0 = not in memory, 1 = in memory */
     int page_table[total_pages];
-    for (int i = 0; i < total_pages; i++)
-    {
+    for (int i = 0; i < total_pages; i++) {
         page_table[i] = 0;
     }
 
-    while (1)
-    {
+    while (1) {
         printf("\nEnter a virtual address (decimal) or -1 to exit: ");
         scanf("%ld", &virt_addr);
-
-        if (virt_addr == -1)
-        {
+        if (virt_addr == -1) {
             break;
         }
-
         long page_number = virt_addr / page_size_bytes;
         long offset      = virt_addr % page_size_bytes;
 
-        if (page_number >= total_pages || page_number < 0)
-        {
+        if (page_number >= total_pages || page_number < 0) {
             printf("Error: Invalid virtual address! Please try again.\n");
             continue;
         }
-
-        if (page_table[page_number] == 0)
-        {
+        if (page_table[page_number] == 0) {
             printf("Page MISS! Loading page %ld into memory...\n", page_number);
             page_table[page_number] = 1;
-        }
-        else
-        {
+        } else {
             printf("Page HIT! Page %ld is already in memory.\n", page_number);
         }
 
@@ -55,7 +41,6 @@ int main(void)
         printf("  Page Number     : %ld\n", page_number);
         printf("  Offset          : %ld\n", offset);
     }
-
     printf("\nExiting simulator.\n");
     return 0;
 }
